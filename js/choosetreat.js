d5073c3c099d7cf0bf98841a0d0e0ca1c072625a
Tharindu Jayakody <140710028+atssamadhi@users.noreply.github.com>
2025-10-17 04:28:55 -0700

    make working carrosal for doct

--- js/choosetreat.js
+++ js/choosetreat.js
@@ -1,2 +1,14 @@
 // choosetreat.js
 console.log("Choose a Treatment section loaded");
+
+// ===== Doctors Carousel 2 =====
+const track2 = document.querySelector('.doctors-carousel-track-2');
+if (track2) {
+  const slides2 = Array.from(track2.children);
+  let currentIndex2 = 0;
+
+  setInterval(() => {
+    currentIndex2 = (currentIndex2 + 1) % slides2.length;
+    track2.style.transform = `translateX(-${currentIndex2 * 100}%)`;
+  }, 2000);
+}
